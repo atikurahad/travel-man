9eb2ffd6e4f3cedbd9087d7dd50606de74710ab7
Atikur Rahaman <atikurahad@gmail.com>
2023-02-21 23:44:41 +0600

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1> Come from Home</h1>
+      <div className='h-96 w-full bg-["../asset/man.jpg"]'>
+
+      </div>
+      <div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Home;
