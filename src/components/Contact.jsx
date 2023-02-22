67a8c3298a698d24df8a017ddcc5da2b398314e1
Atikur Rahaman <atikurahad@gmail.com>
2023-02-22 23:55:28 +0600

    contact are ready

---
+++ src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>I am from Contact</h1>
+      <input type="date" name="date" id="" />
+    </div>
+  );
+};
+
+export default Contact;
