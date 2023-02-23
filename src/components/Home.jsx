77bb4973d31a06f9fc60145aa1e2dd6ddbeeafe5
Atikur Rahaman <atikurahad@gmail.com>
2023-02-24 01:01:23 +0600

    banner added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,8 +4,12 @@ const Home = () => {
   return (
     <div>
       <h1 > Come from Home</h1>
-      <div className='h-96 w-full bg-["../asset/man.jpg"]'>
-
+      <div className="h-[35rem] bg-slate-200 bg-no-repeat bg-cover bg-[url('https://img.trvcdn.net/https://s3.ap-southeast-2.amazonaws.com/travlr.com/cms/prod/Global_ACM/explore-travel-acm.jpg?imgeng=m_box/w_2850/h_1120')]">
+      <section className='text-center grid'>
+          <h1 className='text-4xl'>Get Exclusive Offers Every Week</h1>
+        <p>Sign up to recieve curated and excluive deals & offer straight to  your inbox</p>
+        <button>Sign Up</button>
+      </section>
       </div>
       <div>
 
