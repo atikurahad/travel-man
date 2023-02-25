06885b9d56a986a5ac3e984d4914b8e1d4be2e95
Atikur Rahaman <atikurahad@gmail.com>
2023-02-25 19:57:10 +0600

    banner and text added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
   return (
     <div>
-      <h1 > Come from Home</h1>
-      <div className="h-[35rem] bg-slate-200 bg-no-repeat bg-cover bg-[url('https://img.trvcdn.net/https://s3.ap-southeast-2.amazonaws.com/travlr.com/cms/prod/Global_ACM/explore-travel-acm.jpg?imgeng=m_box/w_2850/h_1120')]">
-      <section className='text-center grid'>
-          <h1 className='text-4xl'>Get Exclusive Offers Every Week</h1>
-        <p>Sign up to recieve curated and excluive deals & offer straight to  your inbox</p>
-        <button>Sign Up</button>
-      </section>
+      <div className=" grid sm:grid-cols-1 h-[45rem] bg-slate-200 bg-no-repeat bg-cover bg-[url('https://img.trvcdn.net/https://s3.ap-southeast-2.amazonaws.com/travlr.com/cms/prod/Global_ACM/explore-travel-acm.jpg?imgeng=m_box/w_2850/h_1120')]">
+        <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+          <section>
+            <h1 className="text-4xl ">Get Exclusive Offers Every Week</h1>
+            <p className="text-4xl mx-auto">
+              Sign up to recieve curated and excluive deals & offer straight to
+              your inbox
+            </p>
+            <button className="text-4xl p-2 text-center font-bold border rounded-xl bg-purple-500 ">Sign Up</button>
+          </section>
+        </div>
       </div>
-      <div>
-
-      </div>
+      <div></div>
     </div>
   );
 };
