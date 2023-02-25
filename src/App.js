12f04c3b65aeb48425e5b11e9377754431c4f7fa
Atikur Rahaman <atikurahad@gmail.com>
2023-02-26 00:51:05 +0600

    delas section added and working on it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import TopDeals from "./components/TopDeals";
 
 function App() {
   return (
     <div >
       <Navbar />
       <Home />
-
+   <TopDeals />
     </div>
   );
 }
---
+++ src/components/TopDeals.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const TopDeals = () => {
+  return (
+    <div>
+      <div >
+        <div className="grid grid-cols-2 mx-[rem]">
+          <img src="https://img.trvcdn.net/https://s3.ap-southeast-2.amazonaws.com/thebalibible.com/uploads/images/venue/8ecf46b8236caae97e9cff6d18ec99a5.jpg?imgeng=m_box/w_1200/h_710" className='h-[25rem] w-[35rem] hover:scale-x-110	' alt="" />
+          <article>
+            <p>Romantic dinner</p>
+            <h1>Charming Candidasa Escape</h1>
+            <p>5 nights, 2adults from</p>
+            <p>USD$ 1500</p>
+          </article>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TopDeals;
