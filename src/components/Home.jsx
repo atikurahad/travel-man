0059b0b4bf7aec1b79c0f5eba6416bcea6018284
Atikur Rahaman <atikurahad@gmail.com>
2023-02-26 00:32:15 +0600

    tour section added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,38 @@
 import React from "react";
 import '../styles/Home.css'
+import traveller from "../asset/traveler.png"
+import luggage from "../asset/luggage.png"
+import consaltact from "../asset/developer.png"
+import travelMan from "../asset/travel.webp"
 const Home = () => {
   return (
     <div>
-   <div className="	 h-[45rem] bg-slate-200  bg-image">
+       <div className="	 h-[45rem] bg-slate-200 ">
+         <img src={travelMan} alt="" srcset="" className="" />
          <div className="my-auto text-gray-600">
-          <p>Travel is the only thing you buy that makes you richer</p>
+          <p>Lorem ipsum dolor sit amet.</p>
           <button className="p-2 bg-red-600">Buy gift here</button>
          </div>
         </div>
+
+          <div className="grid lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-1 items-center mt-5 bg-blue-300 mx-[10rem] p-5 mb-5">
+            <section>
+              <img src={traveller} alt="travel" className="h-[7rem] w-[5rem] mx-auto" />
+              <p className="text-2xl text-center">Travel Now, Pay Later</p>
+              <p className="text-md">Pay for your trip in fonrynightly or monthly installment, interest free..</p>
+            </section>
+            <section className="mx-auto">
+              <img src={consaltact} alt="consaltancy" className="h-[7rem] w-[5rem] mx-auto" />
+              <h1 className="text-2xl  ">Concirage Team</h1>
+              <p className="text-md">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </section>
+            <section>
+              <img src={luggage} alt="travel" className="h-[7rem] w-[5rem] mx-auto" />
+              <h1 className="text-2xl text-center">Boarden Your Horizons</h1>
+              <p className="text-md">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo.</p>
+            </section>
+          </div>
+
       </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const Navbar = () => {
   return (
     <div>
-      <div className="flex justify-between h-[5rem] w-full  p-4 items-center gap-2 font-san bg-white">
-        <p className="text-4xl font-bold text-blue-500">Travel Man</p>
+      <div className="flex justify-between h-[5rem]  p-4 items-center gap-2 font-san bg-blue-900 ">
+        <p className="text-4xl font-bold text-blue-500 mx-[5rem]">Travel Man</p>
         <div className="flex1">
           <ul
-            className=" text-black flex justify-between" >
+            className=" text-white font-mono flex justify-between mx-[5rem]" >
             <li className="text-2xl font-bold mx-2 ">Home</li>
             <li className="text-2xl font-bold mx-2 ">About</li>
             <li className="text-2xl font-bold mx-2">Contact</li>
