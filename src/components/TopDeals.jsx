5cfa81839680a8a47b9dbbb256c70b54f6346e1a
Atikur Rahaman <atikurahad@gmail.com>
2023-02-27 01:24:04 +0600

    material ui and create more component

--- src/components/TopDeals.jsx
+++ src/components/TopDeals.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import tag from "../asset/price-tag.png";
-import MailOutlineIcon from '@mui/icons-material/MailOutline';
-import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
 const TopDeals = () => {
   return (
     <div>
-
-
       <div className="bg-[#F4F4F4] mx-[10rem] mt-5 mb-5 max-w-[1240px]">
         <div className="grid grid-cols-2  justify-items-center	">
           <img
@@ -23,26 +21,31 @@ const TopDeals = () => {
                 <p className="text-xl">5 nights, 2adults from</p>
 
                 <p className="text-2xl text-gray-900">
-                  <span className="text-black font-bold">USD$</span> 1500 </p>
+                  <span className="text-black font-bold">USD$</span> 1500{" "}
+                </p>
                 <div className="flex">
                   <img src={tag} className="h-8" alt="" />
                   <span className="bg-green-200">Tasmania water kingdom</span>
                 </div>
-               </div>
+              </div>
             </div>
-             <div className="mt-10">
-            <button className="border-2 border-solid border-gray-500 m-2 p-3  text-3xl text-center font-mono hover:bg-gray-700 hover:text-white">{<MailOutlineIcon color="error" fontSize="large" />}Enquire Now</button>
-            <button className="m-2 p-3 border-solid border-2 border-gray-500 text-3xl text-center font-mono hover:bg-green-200"> <span className="">{<RemoveRedEyeIcon color="secondary" fontSize="large" />}</span> View Offer</button>
-          </div>
+            <div className="mt-10">
+              <button className="border-2 border-solid border-gray-500 m-2 p-3  text-3xl text-center font-mono hover:bg-gray-700 hover:text-white">
+                {<MailOutlineIcon color="error" fontSize="large" />}Enquire Now
+              </button>
+              <button className="m-2 p-3 border-solid border-2 border-gray-500 text-3xl text-center font-mono hover:bg-green-200">
+                {" "}
+                <span className="">
+                  {<RemoveRedEyeIcon color="secondary" fontSize="large" />}
+                </span>{" "}
+                View Offer
+              </button>
+            </div>
           </article>
-
-
-
-
         </div>
       </div>
 
-       <div className="bg-[#F4F4F4] mx-[10rem] mt-5 mb-5 max-w-[1240px]">
+      <div className="bg-[#F4F4F4] mx-[10rem] mt-5 mb-5 max-w-[1240px]">
         <div className="grid grid-cols-2  justify-items-center	">
           <img
             src="https://img.trvcdn.net/https://s3.ap-southeast-2.amazonaws.com/global.travlr.com/uploads/images/venue/504204686071261eef8afb17a7ef79f4.jpg?imgeng=m_box/w_1200/h_710"
@@ -51,32 +54,38 @@ const TopDeals = () => {
           />
           <article className="mt-5 ">
             <button className="border p-3 ">Barossa valley wine tour</button>
-            <h1 className="text-4xl font-bold">Food and Wine Indulgence in Adelaide</h1>
+            <h1 className="text-4xl font-bold">
+              Food and Wine Indulgence in Adelaide
+            </h1>
 
             <div className="flex  ">
               <div className="flex-row mt-6">
                 <p className="text-xl">4 days, 1adults from</p>
 
                 <p className="text-2xl text-gray-900">
-                  <span className="text-black font-bold">USD$</span> 1280 </p>
+                  <span className="text-black font-bold">USD$</span> 1280{" "}
+                </p>
                 <div className="flex">
                   <img src={tag} className="h-8" alt="" />
                   <span className="bg-green-200">Up to 13% save</span>
                 </div>
-               </div>
+              </div>
             </div>
-             <div className="mt-10">
-            <button className="border-2 border-solid border-gray-500 m-2 p-3  text-3xl text-center font-mono hover:bg-gray-700 hover:text-white">{<MailOutlineIcon color="error" fontSize="large" />}Enquire Now</button>
-            <button className="m-2 p-3 border-solid border-2 border-gray-500 text-3xl text-center font-mono hover:bg-green-200"> <span className="">{<RemoveRedEyeIcon color="secondary" fontSize="large" />}</span> View Offer</button>
-          </div>
+            <div className="mt-10">
+              <button className="border-2 border-solid border-gray-500 m-2 p-3  text-3xl text-center font-mono hover:bg-gray-700 hover:text-white">
+                {<MailOutlineIcon color="error" fontSize="large" />}Enquire Now
+              </button>
+              <button className="m-2 p-3 border-solid border-2 border-gray-500 text-3xl text-center font-mono hover:bg-green-200">
+                {" "}
+                <span className="">
+                  {<RemoveRedEyeIcon color="secondary" fontSize="large" />}
+                </span>{" "}
+                View Offer
+              </button>
+            </div>
           </article>
-
         </div>
       </div>
-
-
-
-
     </div>
   );
 };
