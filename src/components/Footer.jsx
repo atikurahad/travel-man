e6c828a29e5fd54334b3c5f5c6a5a12bbbd13357
Atikur Rahaman <atikurahad@gmail.com>
2023-03-07 01:54:37 +0600

    footer completed successfully

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,44 +8,51 @@ import { blue, red } from "@mui/material/colors";
 
 const Footer = () => {
   return (
-    <div>
-      <div className="h-[20rem] bg-[#00173F] grid grid-cols-4">
+    <div className=" bg-[#00173F] h-full">
+      <div className=" grid lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-4">
         <section className="mt-12">
           <h1 className="text-4xl text-white text-center">Countries</h1>
-         <span className="flex flex-col items-center mt-5">
-           <a href="/">Australia</a>
-          <a href="/">Thailand</a>
-          <a href="/">Indonesia</a>
-          <a href="/">Bangladesh</a>
-         </span>
+          <span className="flex flex-col items-center mt-5">
+            <a href="/">Australia</a>
+            <a href="/">Thailand</a>
+            <a href="/">Indonesia</a>
+            <a href="/">Bangladesh</a>
+          </span>
         </section>
         <section className="mt-12">
           <h1 className="text-4xl text-white text-center">Contact Us</h1>
-         <span className="flex flex-col items-center mt-5">
-           <a href="/">Terms</a>
-          <a href="/">Refund Policy</a>
-          <a href="/">Call Us:v1322uy2u3t3</a>
-          <a href="/">Claim your listing</a>
-         </span>
+          <span className="flex flex-col items-center mt-5">
+            <a href="/">Terms</a>
+            <a href="/">Refund Policy</a>
+            <a href="/">Call Us:v1322uy2u3t3</a>
+            <a href="/">Claim your listing</a>
+          </span>
         </section>
         <section className="mt-12">
           <h1 className="text-4xl text-white text-center">Privacy Policy</h1>
           <span className="flex flex-col items-center mt-5">
             <a href="/">Our Blog</a>
-          <a href="/">DMCA</a>
-          <a href="/">Manage Booking</a>
-          <a href="/">Comaplaints Policy</a>
+            <a href="/">DMCA</a>
+            <a href="/">Manage Booking</a>
+            <a href="/">Comaplaints Policy</a>
           </span>
         </section>
         <section>
-          <span >
-            <FacebookSharpIcon  sx={{color:blue[400], fontSize: "80px"}} />
-            <YouTubeIcon sx={{color:red[800], fontSize: "80px"}}  />
-            <TwitterIcon sx={{color:blue[400], fontSize: "80px"}}  />
-            <Instagram sx={{color:red[600], fontSize: "80px"}}  />
+          <span className="flex justify-items-cener mt-8">
+            <FacebookSharpIcon sx={{ color: blue[400], fontSize: "80px" }} />
+            <YouTubeIcon sx={{ color: red[800], fontSize: "80px" }} />
+            <TwitterIcon sx={{ color: blue[400], fontSize: "80px" }} />
+            <Instagram sx={{ color: red[600], fontSize: "80px" }} />
           </span>
         </section>
+
       </div>
+      <hr className="my-4 mx-[12rem]" />
+       <p className="text-xl text-white text-center p-5">
+          {" "}
+          <span className="text-white font-bold text-2xl font-serif">&copy;</span> All rights Reserved{" "}
+          <span className="text-2xl font-bold text-blue-600">Travel Man</span>
+        </p>
     </div>
   );
 };
